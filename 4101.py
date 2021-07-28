3ed62cfda0b8f7e30d75e1022c8b0fec4795ac07
dlwlstjd1205 <dlwlstjd1205@gmail.com>
2021-07-28 17:56:15 +0900

    백준 4101문제(크냐?)

---
+++ 4101.py
@@ -0,0 +1,9 @@
+while True:
+    num1, num2 = map(int,input().split())
+    if num1 == 0 and num2 == 0:
+        break
+    else:
+        if num1 > num2:
+            print("Yes")
+        else:
+            print("No")
