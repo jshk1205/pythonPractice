b357cb7f6d9a9b191ba88cd0e76b8b7bd37a0d93
dlwlstjd1205 <dlwlstjd1205@gmail.com>
2021-08-03 16:27:37 +0900

    백준 2609문제(최대공약수와 최소공배수)

---
+++ 2609.py
@@ -0,0 +1,13 @@
+num1, num2 = map(int, input().split())
+if num1 < num2:
+    temp = num1
+    num1 = num2
+    num2 = temp
+a, b = num1, num2
+while num2 != 0:
+    r= num1 % num2
+    num1, num2 = num2, r
+    factor = num1
+    multi = (a * b) // num1
+print(factor)
+print(multi)
