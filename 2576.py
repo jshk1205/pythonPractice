940498e157859e843480c84471b0980642d5339b
dlwlstjd1205 <dlwlstjd1205@gmail.com>
2021-08-06 16:11:24 +0900

    백준 2576문제(홀수)

--- 2576.py
+++ 2576.py
@@ -1,9 +1,9 @@
 num =[]
+sum = 0
+min_list = []
 for _ in range(0,7):
     n = int(input())
     num.append(n)
-sum = 0
-min_list = []
 for i in range(0, len(num)):
     if num[i] % 2 !=0:
         sum += num[i]
